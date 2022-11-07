0c54b698e6cee50837d44e374871bd7086552a94
Steve Kinney <hello@stevekinney.net>
2022-11-07 07:15:04 -0700

    Solution: useState and Accident Counter

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [draftCount, setDraftCount] = useState<string | number>(0);
+
+  useEffect(() => {
+    setDraftCount(count);
+  }, [count]);
 
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
@@ -13,8 +18,17 @@ const Counter = () => {
         <button onClick={() => setCount(count + 1)}>➕ Increment</button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setCount(Number(draftCount));
+          }}
+        >
+          <input
+            type="number"
+            value={draftCount}
+            onChange={(e) => setDraftCount(e.target.value)}
+          />
           <button type="submit">Update Counter</button>
         </form>
       </div>
