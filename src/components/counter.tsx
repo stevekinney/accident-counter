363f4c7bd4ef55ca8ebe7f327dbe6f09119db8be
Steve Kinney <hello@stevekinney.net>
2022-11-07 06:28:24 -0700

    Implement basic state

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => setCount(count - 1)}>➖ Decrement</button>
+        <button onClick={() => setCount(0)}>🔁 Reset</button>
+        <button onClick={() => setCount(count + 1)}>➕ Increment</button>
       </div>
       <div>
         <form onSubmit={(e) => e.preventDefault()}>
