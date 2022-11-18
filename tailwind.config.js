b4d478d75ff3bfc189deeb819d76568f17dd86d6
Steve Kinney <hello@stevekinney.net>
2022-11-18 09:39:59 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      colors: { ...colors, primary: colors.orange },
+      colors: { ...colors, primary: colors.red },
     },
   },
   plugins: [],
